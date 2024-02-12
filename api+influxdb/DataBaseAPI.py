bbc2863659056b43c17c41c13fc3b3542f48b85a
Aron Seidl <aron.seidl@zeiss.com>
2024-02-12 16:08:10 +0100

    GET-Routen zur Validierung hinzugefügt

--- api+influxdb/DataBaseAPI.py
+++ api+influxdb/DataBaseAPI.py
@@ -1,10 +1,42 @@
 from flask import Flask, request, jsonify
 from influxdb import InfluxDBClient
+import requests
+import socket
 
 app = Flask(__name__)
 influx_client = InfluxDBClient(host="influxdb", database='DHBW_Blickbox')
 
-@app.route('/iot/pingdb', methods=['GET'])
+@app.route('/iot/api/pingBB', methods=['GET'])
+def pingBlickBox():
+    ip_address = '172.19.80.1'
+    port = 22
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)  
+        s.connect((ip_address, port))
+        s.close() 
+        return jsonify({"message": "BlickBox ist erreichbar"}), 200
+    except Exception as e:
+        return jsonify({"Fehler": "Keine Verbindung zur Blickbox"}), 503
+
+
+
+@app.route('/iot/api/pingGF', methods=['GET'])
+def pingGrafana():
+    url = "http://grafana-server:3000/api/health"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return jsonify({"message": "Verbingung zu Grafana steht"}), 200
+        else:
+            return jsonify({"message": "Grafana Server hat geantwortet mit Statuscode {response.status_code}"}), response.status_code
+    except requests.ConnectionError:
+        return jsonify({"Fehler": "Keine Verbindung zum Grafana Server"}), 503
+
+
+
+
+@app.route('/iot/api/pingDB', methods=['GET'])
 def pingthis():
     try:
         influx_client.ping()
