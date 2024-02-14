dc10c53e9f8430f63c4397fcc3a87437b159e9e8
Maximilian Müller <63018152+mxmueller@users.noreply.github.com>
2024-02-14 18:16:31 +0100

    #79 + #64 Mockup und Komponentenplanung fe

--- valentin/build.sh
+++ valentin/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo chown -R $(whoami) ~/.docker
+
 docker build -t valentin .
 docker run -d -p 4000:80 --name valentin valentin
 
--- valentin/origin/src/App.js
+++ valentin/origin/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { ChakraProvider } from "@chakra-ui/react"
 import { Button } from "@chakra-ui/react"
