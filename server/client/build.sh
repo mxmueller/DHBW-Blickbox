18acec987f33032746998271120a293529bb2dbc
Maximilian Müller <63018152+mxmueller@users.noreply.github.com>
2024-02-18 14:37:12 +0100

    #131-fixed-ports

--- server/client/build.sh
+++ server/client/build.sh
@@ -4,10 +4,17 @@ echo VALENTIN BUILD SETUP
 echo ------------------------
 
 cd ./valentin
+npm install 
 
-npm install
-npm audit fix --force
+echo ------------------------
+echo TESTING VALENTIN BEFORE DEPLYOMENT
+echo ------------------------
+
 npm test -- --watchAll=false
+
+echo ------------------------
+echo DEPLOYING DOCKER
+echo ------------------------
 
 docker-compose up --build
 
