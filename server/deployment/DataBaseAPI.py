a1a94d00cf8eb0956aad8cd95f9a23b990040185
Aron Seidl <aron.seidl@zeiss.com>
2024-02-20 19:37:07 +0100

    #136-Streaming-Websocket-erstellen2

--- server/deployment/DataBaseAPI.py
+++ server/deployment/DataBaseAPI.py
@@ -1,21 +1,17 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, url_for
 from flask_sock import Sock
 from influxdb import InfluxDBClient
 import requests
 import socket
+from datetime import datetime
+from RingBuffer import *
 
 app = Flask(__name__)
 influx_client = InfluxDBClient(host="influxdb", database='DHBW_Blickbox')
 sock = Sock(app)
 sock.init_app(app)
 
-
-@sock.route('/log-stream')
-def logstream(sock):
-    try:
-        sock.send("Hello world")
-    except Exception as e:
-        print(e)
+ringBuffer = RingBuffer(100)
 
 def return_response(message, value, status_code):
     data = {message: value}
@@ -23,52 +19,81 @@ def return_response(message, value, status_code):
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
+
+@sock.route('/log-stream')
+def logstream(sock):
+    global last_sent_log
+    while True:
+        try:
+            check_and_send_new_entries(ringBuffer, sock)
+        except Exception as e:
+            print(e)
+
+
 @app.route('/iot/api/pingBB', methods=['GET'])
 def pingBlickBox():
     ip_address = '172.19.80.1'
     port = 22
+    log(title='Called', message=url_for('pingBlickBox'), type='info', ringbuffer=ringBuffer)
+    log(title='Versuch', message='Versuche Verbindung zur Blickbox herzusetellen', type='info', ringbuffer=ringBuffer)
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(1)  
         s.connect((ip_address, port))
         s.close() 
+        log(title='Connected', message='Verbindung zur BlickBox hergestellt', type='success', ringbuffer=ringBuffer)
         return return_response("message", "BlickBox ist erreichbar", 200)
     except Exception as e:
+        log(title='Keine Verbindung', message='Keine Verbindung zur Blickbox', type='error', ringbuffer=ringBuffer)
         return return_response("Fehler", "Keine Verbindung zur Blickbox", 503)
 
 
 @app.route('/iot/api/pingGF', methods=['GET'])
 def pingGrafana():
     url = "http://grafana-server:3000/api/health"
+    log(title='Called', message=url_for('pingGrafana'), type='info', ringbuffer=ringBuffer)
+    log(title='Versuch', message='Versuche Verbindung mit Grafana herzusetellen', type='info', ringbuffer=ringBuffer)
     try:
         response = requests.get(url)
         if response.status_code == 200:
+            log(title='Connected', message='Verbindung zu Grafana hergestellt', type='success', ringbuffer=ringBuffer)
             return return_response("message", "Verbingung zu Grafana steht", 200)
         else:
-            return return_response("message", "Grafana Server hat geantwortet mit Statuscode {response.status_code}", response.status_code)
+            log(title='Info', message=f'Grafana Server hat geantwortet mit Statuscode {response.status_code}', type='info', ringbuffer=ringBuffer)
+            return return_response("message", f'Grafana Server hat geantwortet mit Statuscode {response.status_code}', response.status_code)
     except requests.ConnectionError:
+        log(title='Keine Verbindung', message='Keine Verbindung zum Grafana Server', type='error', ringbuffer=ringBuffer)
         return return_response("Fehler", "Keine Verbindung zum Grafana Server", 503)
 
 
 
 @app.route('/iot/api/pingDB', methods=['GET'])
 def pingthis():
+    log(title='Called', message=url_for('pingthis'), type='info', ringbuffer=ringBuffer)
+    log(title='Versuch', message='Versuche Verbindung zur Datenbank herzusetellen', type='info', ringbuffer=ringBuffer)
+
     try:
         influx_client.ping()
+        log(title='Connected', message='Verbindung zur Datenbank hergestellt', type='success', ringbuffer=ringBuffer)
         return return_response("message", "Verbingung zur Datenbank steht", 200)
     except Exception as e:
+        log(title='Exception', message=str(e), type='error', ringbuffer=ringBuffer)
         return return_response("error", str(e), 500)
 
 
 
 @app.route('/iot/api/insert/temperature', methods=['POST'])
 def insert_temperature():
+    log(title='Called', message=url_for('pingthis'), type='info', ringbuffer=ringBuffer)
+    log(title='Versuch', message='Versuche Temperatur-Wert einzufügen', type='info', ringbuffer=ringBuffer)
     try:
         data = request.json
         if 'temperature' not in data:
+            log(title='Exception', message="Key der Eingabe war nicht 'temperature'", type='error', ringbuffer=ringBuffer)
             return return_response("message", "Falscher Input!", 400)
         temperature = float(data['temperature'])
         if(temperature < -60.0 or temperature > 100.0):
+            log(title='Exception', message=f'Wert der Temperatur stimmt nicht. Wert: {temperature}', type='error', ringbuffer=ringBuffer)
             return return_response("message", "Falscher Input! Temperatur nicht in Range", 400)
         json_body = [
             {
@@ -79,8 +104,10 @@ def insert_temperature():
             }
         ]
         influx_client.write_points(json_body)
+        log(title='Info', message='Daten wurden erfolgreich eingefügt!', type='success', ringbuffer=ringBuffer)
         return return_response("message", "Daten erfolgreich eingefügt!", 200)
     except Exception as e:
+        log(title='Exception', message=str(e), type='error', ringbuffer=ringBuffer)
         return return_response("error", str(e), 500)
 
 if __name__ == '__main__':
