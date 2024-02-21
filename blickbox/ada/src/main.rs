d3b06557291673a42890853442574dbbdc0b596a
vv-ab <vivianberger028@gmail.com>
2024-02-21 21:21:08 +0100

    #119-Erweiterte-Abdeckung-Datenabfrage und Refactoring

--- blickbox/ada/src/main.rs
+++ blickbox/ada/src/main.rs
@@ -1,85 +1,104 @@
 mod sensor_modules;
 
 use std::{io, thread};
-use std::fs::OpenOptions;
+use std::fs::{File, OpenOptions};
 
 use std::io::{Read, Write};
 use std::time::{Duration, SystemTime};
 use chrono::{DateTime, Utc};
 use chrono_tz::Europe::Berlin;
+use serialport::SerialPort;
 use tokio::time;
+use crate::sensor_modules::dht_temp_hum::dht_sensor::get_dht_data;
 
-//use crate::sensor_modules;
+type Error = String;
+type Result<T> = std::result::Result<T, Error>;
 
+#[derive(Clone, Debug)]
+pub struct SensorData {
+    timestamp: String,
+    temperature: String,
+    humidity: String,
+    wind_direction: i32,
+    wind_speed: f32,
+    precipitation_amount: f32,
+}
 
 #[tokio::main]
 async fn main() {
+
+    if let Err(error) = execute().await {
+        // here it should send the error to the cloud for it to be noted or fixed
+        eprintln!("{}", error);
+    }
+}
+
+async fn execute() -> Result<()> {
+
     // Opens file in append mode (and creating it if it doesn't exist)
     let mut file = OpenOptions::new()
         .write(true)
         .append(true)
         .create(true)
         .open("command_history.txt").unwrap();
+
+    // seeing available ports in terminal
     let ports = serialport::available_ports().expect("No ports found!");
     for p in ports {
         println!("{}", p.port_name);
     }
 
-    let mut port = serialport::new("/dev/ttyACM0", 115200)
+    let mut port: Box<dyn SerialPort> = serialport::new("/dev/ttyACM0", 115200)
         .timeout(Duration::from_millis(1000))
-        .open().expect("Failed to open port");
+        .open()
+        .map_err(|error| format!("Failed to open port: {:?}", error))?;
 
     // Initial delay before sending the first command
     tokio::time::sleep(Duration::from_secs(5)).await;
 
     tokio::time::sleep(Duration::from_millis(5000)).await;
 
-    let mut serial_buf: Vec<u8> = vec![0; 100];
-
-
-    //handle_dht_sensor_data();
-
     // Timer interval for sending commands (every 30 minutes)
-    let mut interval = time::interval(Duration::from_secs(120));
+    let mut interval = time::interval(Duration::from_secs(20));
+
+    let time = get_time();
+
+    let mut sensor_data = SensorData {
+        timestamp: time,
+        temperature: String::new(),
+        humidity: String::new(),
+        wind_direction: 0,
+        wind_speed: 0.0,
+        precipitation_amount: 0.0,
+    };
 
     loop {
         interval.tick().await;
 
-        // here data is requested
-        let temperature_command = "t\n";
-        port.write_all(temperature_command.as_ref()).expect("Failed to send command");
+        let mut port = port.try_clone().map_err(|error| format!("{:?}", error))?;
 
-        match port.read(serial_buf.as_mut_slice()) {
-            Ok(t) => {
-                let received_data = &serial_buf[..t];
-                let interpreted_data = interpret_data(received_data);
+        let temperature = get_dht_data("t\n", &mut port).await?;
+        let humidity = get_dht_data("h\n", &mut port).await?;
 
-                // writing the received data to file system
-                // data to be written should be a struct sooner or later... :/
-                if !interpreted_data.is_empty() {
-                    let date_time_format: DateTime<Utc> = SystemTime::now().into();
-                    let time = date_time_format.with_timezone(&Berlin).format("%Y-%m-%d %H:%M:%S").to_string();
-                    let data = format!("{:?}, {:?}\n", time, interpreted_data);
-                    file.write_all(data.as_bytes()).unwrap();
-                }
+        sensor_data.temperature = temperature;
+        sensor_data.humidity = humidity;
 
-                tokio::time::sleep(Duration::from_millis(1000)).await;
-                continue;
-            }
-            Err(ref e) if e.kind() == io::ErrorKind::TimedOut => {
-                println!("Timeout occurred");
-                continue;
-            }
-            Err(e) => {
-                eprintln!("Failed to read from port: {}", e);
-                break;
-            }
-        };
+        write_to_file(&file, &sensor_data);
+
+        // handle_weather_station_data();
     }
+}
 
-    println!("why");
-    tokio::time::sleep(Duration::from_millis(8000)).await;
-    print!("is is is");
+pub fn get_time() -> String {
+    let date_time_format: DateTime<Utc> = SystemTime::now().into();
+    let time = date_time_format.with_timezone(&Berlin).format("%Y-%m-%d %H:%M:%S").to_string();
+    return time;
+}
+pub fn write_to_file(mut file: &File, sensor_data: &SensorData) {
+    // writing the received data to file system
+    // data to be written should be a struct sooner or later... :/
+        let data = format!("{:?}\n", sensor_data);
+        file.write_all(data.as_bytes()).unwrap();
 }
 
 pub fn interpret_data(data: &[u8]) -> String {
@@ -108,10 +127,14 @@ pub fn interpret_data(data: &[u8]) -> String {
 
             // if there would be more commands in the received_data
             // ...I'm not sure it would catch the correct data as interpreted_data everytime
-            if separated_string_by_colon.len() >= 2 {
-                println!("Interpreted data: {:?}", separated_string_by_colon[1].clone().to_string());
-                separated_string_by_colon[1].clone().to_string()
+            if separated_string_by_colon.len() == 2 {
+                println!("Interpreted data: {:?}", separated_string_by_colon[1].to_string());
+                separated_string_by_colon[1].to_string()
+            } else if separated_string_by_colon.len() > 2 {
+                println!("Interpreted data: {:?}", separated_string_by_colon[separated_string_by_colon.len() - 1 ].to_string());
+                separated_string_by_colon[separated_string_by_colon.len() - 1 ].to_string()
             } else {
+                // no clue what the received data could be in this case
                 println!("damn, didnt work");
                 String::new()
             }
--- blickbox/ada/src/sensor_modules/dht_temp_hum.rs
+++ blickbox/ada/src/sensor_modules/dht_temp_hum.rs
@@ -1,6 +1,34 @@
 pub mod dht_sensor {
+    use std::{io, thread};
+    use std::time::Duration;
+    use serialport::SerialPort;
+    use crate::interpret_data;
 
+    pub async fn get_dht_data(command: &str, mut port: &mut Box<dyn SerialPort>) -> crate::Result<String> {
 
-    //handle_dht_sensor_data() { ... }
+        let mut serial_buf: Vec<u8> = vec![0; 100];
 
+        // here data is requested
+        if command == "h\n" || command == "t\n" {
+            port.write_all(command.as_ref()).expect("Failed to send command");
+        }
+        tokio::time::sleep(Duration::from_millis(1000)).await;
+
+        match port.read(serial_buf.as_mut_slice()) {
+            Ok(t) => {
+                let received_data = &serial_buf[..t];
+                let interpreted_data = interpret_data(received_data);
+                serial_buf.clear();
+                Ok(interpreted_data)
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::TimedOut => {
+                serial_buf.clear();
+                Err(format!("Timeout occured. {:?}", e))
+            }
+            Err(e) => {
+                serial_buf.clear();
+                Err(format!("Failed to read from port: {}", e))
+            }
+        }
+    }
 }
