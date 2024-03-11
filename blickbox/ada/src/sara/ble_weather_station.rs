3c6958f17f0513563c9f4c682f7907e4cbed1f99
vv-ab <vivianberger028@gmail.com>
2024-03-11 16:56:56 +0100

    #169-BLE-ADA-Integration

--- blickbox/ada/src/sara/ble_weather_station.rs
+++ blickbox/ada/src/sara/ble_weather_station.rs
@@ -1,21 +1,181 @@
 pub mod ble_weather_station {
-    use std::collections::HashSet;
     use std::time::Duration;
     use btleplug::api::{bleuuid::uuid_from_u16, Central, CharPropFlags, Manager as _, Peripheral as _, ScanFilter};
-    use btleplug::platform::{Adapter, Manager, Peripheral};
+    use btleplug::platform::{Manager, Peripheral};
     use tokio::time;
     use futures_util::stream::StreamExt;
 
     use uuid::Uuid;
-
+    use crate::SensorData;
 
     // Define the UUIDs for the BLE characteristics you want to interact with
     const TEMP_NOTIFY_CHARACTERISTICS_UUID: Uuid = uuid_from_u16(0x2101);
     const HUMIDITY_NOTIFY_CHARACTERISTICS_UUID: Uuid = uuid_from_u16(0x2102);
+    const RAINFALL_NOTIFY_CHARACTERISTICS_UUID: Uuid = uuid_from_u16(0x2203);
+    const WIND_DIRECTION_NOTIFY_CHARACTERISTICS_UUID: Uuid = uuid_from_u16(0x2201);
+    const WIND_SPEED_NOTIFY_CHARACTERISTICS_UUID: Uuid = uuid_from_u16(0x2202);
     // Define the UUID for the service containing the characteristics
-    const SERVICE_UUID: Uuid = uuid_from_u16(0x1101);
+    const AIR_SERVICE_UUID: Uuid = uuid_from_u16(0x1101);
+    const WEATHER_STATION_SERVICE_UUID: Uuid = uuid_from_u16(0x1102);
 
-    pub async fn get_data_ble() -> crate::Result<()> {
+    // gotta be 5 later on for production
+    const DESIRED_NOTIFICATION_COUNT: i32 = 2;
+
+    pub async fn get_data_ble(peripheral: Peripheral, sensor_data: &mut SensorData) -> crate::Result<()> {
+        // Discover possible services from peripheral device
+        peripheral.discover_services()
+            .await
+            .map_err(|_| String::from("Failed to discover services of peripheral"))?;
+
+        // Find the temperature characteristics
+        let temp_characteristic = peripheral
+            .characteristics()
+            .iter()
+            .find(|&c| c.uuid == TEMP_NOTIFY_CHARACTERISTICS_UUID && c.properties.contains(CharPropFlags::NOTIFY))
+            .unwrap().clone();
+
+        // Subscribe to temperature notification
+        peripheral
+            .subscribe(&temp_characteristic)
+            .await
+            .map_err(|_| String::from("Failed to subscribe to humidity notifications"))?;
+
+        // Find the humidity characteristic
+        let humidity_characteristic = peripheral
+            .characteristics()
+            .iter()
+            .find(|&c| c.uuid == HUMIDITY_NOTIFY_CHARACTERISTICS_UUID && c.properties.contains(CharPropFlags::NOTIFY))
+            .unwrap().clone();
+
+        // Subscribe humidity value
+        peripheral
+            .subscribe(&humidity_characteristic)
+            .await
+            .map_err(|_| String::from("Failed to subscribe to humidity notifications"))?;
+
+        // Find the wind speed characteristics
+        let ws_characteristic = peripheral
+            .characteristics()
+            .iter()
+            .find(|&c| c.uuid == WIND_SPEED_NOTIFY_CHARACTERISTICS_UUID && c.properties.contains(CharPropFlags::NOTIFY))
+            .unwrap().clone();
+
+        // Subscribe to wind speed notification
+        peripheral
+            .subscribe(&ws_characteristic)
+            .await
+            .map_err(|_| String::from("Failed to subscribe to wind speed notifications"))?;
+
+
+        // Find the wind direction characteristics
+        let wd_characteristic = peripheral
+            .characteristics()
+            .iter()
+            .find(|&c| c.uuid == WIND_DIRECTION_NOTIFY_CHARACTERISTICS_UUID && c.properties.contains(CharPropFlags::NOTIFY))
+            .unwrap().clone();
+
+        // Subscribe to wind direction notification
+        peripheral
+            .subscribe(&wd_characteristic)
+            .await
+            .map_err(|_| String::from("Failed to subscribe to wind direction notifications"))?;
+
+
+        // Find the rainfall characteristics
+        let rfm_characteristic = peripheral
+            .characteristics()
+            .iter()
+            .find(|&c| c.uuid == RAINFALL_NOTIFY_CHARACTERISTICS_UUID && c.properties.contains(CharPropFlags::NOTIFY))
+            .unwrap().clone();
+
+        // Subscribe to rainfall notification
+        peripheral
+            .subscribe(&rfm_characteristic)
+            .await
+            .map_err(|_| String::from("Failed to subscribe to rainfall notifications"))?;
+
+        // Process notifications as they are received
+        let mut notification_stream = peripheral
+            .notifications()
+            .await
+            .map_err(|_| String::from("Failed to get notification stream"))?;
+
+        // Define a counter to limit the number of notifications processed
+        let mut notification_count = 0;
+
+        let mut got_temp_value = false;
+        let mut got_hum_value = false;
+        let mut got_ws_value = false;
+        let mut got_wd_value = false;
+        let mut got_rfm_value = false;
+
+        // Process while the BLE connection is not broken or stopped.
+        while let Some(data) = notification_stream.next().await {
+            match data.uuid {
+                TEMP_NOTIFY_CHARACTERISTICS_UUID => {
+                    if !got_temp_value {
+                        // Handle temperature notification
+                        let temperature_value = data.value[0] as f32;
+                        println!("Temperature: {}", temperature_value);
+                        sensor_data.temperature = temperature_value;
+                        notification_count += 1;
+                        got_temp_value = true;
+                    }
+                }
+                HUMIDITY_NOTIFY_CHARACTERISTICS_UUID => {
+                    if !got_hum_value {
+                        // Handle temperature notification
+                        let humidity_value = data.value[0] as f32;
+                        println!("Humidity: {}", humidity_value);
+                        sensor_data.humidity = humidity_value;
+                        notification_count += 1;
+                        got_hum_value = true;
+                    }
+                }
+                RAINFALL_NOTIFY_CHARACTERISTICS_UUID => {
+                    if !got_rfm_value {
+                        // Handle temperature notification
+                        let rainfall_value = data.value[0] as f32;
+                        println!("Rainfall: {}", rainfall_value);
+                        sensor_data.precipitation_amount = rainfall_value;
+                        notification_count += 1;
+                        got_rfm_value = true;
+                    }
+                }
+                WIND_DIRECTION_NOTIFY_CHARACTERISTICS_UUID => {
+                    if !got_wd_value {
+                        // Handle temperature notification
+                        let wd_value = data.value[0] as f32;
+                        println!("Wind direction: {}", wd_value);
+                        sensor_data.wind_direction = wd_value;
+                        notification_count += 1;
+                        got_wd_value = true;
+                    }
+                }
+                WIND_SPEED_NOTIFY_CHARACTERISTICS_UUID => {
+                    if !got_ws_value {
+                        // Handle temperature notification
+                        let ws_value = data.value[0] as f32;
+                        println!("Wind speed: {}", ws_value);
+                        sensor_data.wind_speed = ws_value;
+                        notification_count += 1;
+                        got_ws_value = true;
+                    }
+                }
+                _ => {
+                    println!("Unknown data received °o°");
+                }
+            }
+            // Check if the desired number of notifications have been processed
+            if notification_count >= DESIRED_NOTIFICATION_COUNT {
+                break; // Exit the loop if desired count is reached
+            }
+        }
+
+        Ok(())
+    }
+
+    pub async fn connect_peripheral_device() -> crate::Result<Peripheral> {
         // Initialize the Bluetooth manager
         let manager = Manager::new().await.unwrap();
 
@@ -65,59 +225,8 @@ pub mod ble_weather_station {
         peripheral.connect()
             .await
             .map_err(|_| String::from("Failed to connect to peripheral"))?;
-        // Discover possible services from peripheral device
-        peripheral.discover_services()
-            .await
-            .map_err(|_| String::from("Failed to discover services of peripheral"))?;
 
-        // Find the temperature characteristics
-        let temp_characteristic = peripheral
-            .characteristics()
-            .iter()
-            .find(|&c| c.uuid == TEMP_NOTIFY_CHARACTERISTICS_UUID && c.properties.contains(CharPropFlags::NOTIFY))
-            .unwrap().clone();
-
-        // Find the humidity characteristic
-        let humidity_characteristic = peripheral
-            .characteristics()
-            .iter()
-            .find(|&c| c.uuid == HUMIDITY_NOTIFY_CHARACTERISTICS_UUID && c.properties.contains(CharPropFlags::NOTIFY))
-            .unwrap().clone();
-
-        // Subscribe to temperature notification
-        peripheral
-            .subscribe(&temp_characteristic)
-            .await
-            .map_err(|_| String::from("Failed to subscribe to humidity notifications"))?;
-
-        // Read humidity value
-        peripheral
-            .subscribe(&humidity_characteristic)
-            .await
-            .map_err(|_| String::from("Failed to subscribe to humidity notifications"))?;
-
-        // Convert the values to appropriate types (assuming they are encoded appropriately)
-        // Process notifications as they are received
-        let mut notification_stream = peripheral
-            .notifications()
-            .await
-            .map_err(|_| String::from("Failed to get notification stream"))?;
-
-        // Process while the BLE connection is not broken or stopped.
-        while let Some(data) = notification_stream.next().await {
-            tokio::time::sleep(Duration::from_secs(5)).await;
-            println!(
-                "Received data from {:?} with uuid: {:?} and value {:?}",
-                peripheral.properties().await.unwrap().unwrap().local_name, data.uuid, data.value
-            );
-        };
-
-        // Disconnect from the peripheral
-        peripheral.disconnect()
-            .await
-            .map_err(|_| String::from("Failed to disconnect from peripheral"))?;
-
-        Ok(())
+        Ok(peripheral)
     }
 }
 
--- blickbox/ada/src/sara/weather_station.rs
+++ blickbox/ada/src/sara/weather_station.rs
@@ -2,7 +2,6 @@ pub mod weather_station {
     use std::io;
     use std::time::Duration;
     use serialport::SerialPort;
-    use crate::interpret_data;
 
     pub async fn get_weather_station_data(command: &str, mut port: &mut Box<dyn SerialPort>) -> crate::Result<String> {
 
@@ -29,6 +28,39 @@ pub mod weather_station {
                 serial_buf.clear();
                 Err(format!("Failed to read from port: {}", e))
             }
+        }
+    }
+
+    pub fn interpret_data(data: &[u8]) -> String {
+        /// no error handling lol, no risk no fun
+        // here i am interpreting the received string before returning it :)
+        // : als Seperator zwischen Key und Value der Werte und String wird mit \n beendet und weitere cases werden abgedeckt
+        let mut string = String::from_utf8_lossy(data).to_string();
+
+        println!("received data to be interpreted: {:?}", string);
+
+        // Remove trailing \r\n or \n characters
+        if string.ends_with("\n") {
+            string.pop();
+        }
+        if string.ends_with("\r") {
+            string.pop();
+        }
+
+        let separated_string_by_colon = string.split(':').collect::<Vec<&str>>();
+
+        // if there would be more commands in the received_data
+        // ...I'm not sure it would catch the correct data as interpreted_data everytime
+        if separated_string_by_colon.len() == 2 {
+            println!("Interpreted data: {:?}", separated_string_by_colon[1].to_string());
+            return separated_string_by_colon[1].to_string()
+        } else if separated_string_by_colon.len() > 2 {
+            println!("Interpreted data: {:?}", separated_string_by_colon[separated_string_by_colon.len() - 1].to_string());
+            return separated_string_by_colon[separated_string_by_colon.len() - 1].to_string()
+        } else {
+            // no clue what the received data could be in this case
+            println!("damn, didnt work");
+            return String::new()
         }
     }
 }
