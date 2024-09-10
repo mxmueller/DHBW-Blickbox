229f99cf280f6534ca76ce4cc0b2e12cb876ad80
Maximilian Müller <63018152+mxmueller@users.noreply.github.com>
2024-09-10 15:04:38 +0200

    fix: test CI/CD

--- server/client/valentin/src/dashboard/compontents/settings/settings.test.js
+++ server/client/valentin/src/dashboard/compontents/settings/settings.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import Settings from "./Settings";
+import Settings from "./settings";
 
 jest.mock("./settings.alerts.js", () => () => <div>Mock AlertSettings</div>);
 
