cb9bb45957f32a5d13852aa8fbdafae2e7714c37
Aron Seidl <aron.seidl@zeiss.com>
2024-09-13 19:55:55 +0200

    Fixed Test values

--- server/deployment/tests/API-Tests.py
+++ server/deployment/tests/API-Tests.py
@@ -525,7 +525,7 @@ def test_batteryv_edge_cases_positve(skip_batteryv_tests, battery_voltage):
     assert response.json() == {"message": "Falscher Input! Batteriespannungs Wert nicht in Range"}
 
 @pytest.mark.usefixtures("skip_batteryv_tests")
-@pytest.mark.parametrize("battery_voltage", [20.4, 99.9, 50.0])
+@pytest.mark.parametrize("battery_voltage", [0.4, 1.9, 3.0])
 def test_batteryv_correct_values(skip_batteryv_tests, battery_voltage):
     if skip_batteryv_tests:
         pytest.skip("No-BatteryV Option gesetzt")
