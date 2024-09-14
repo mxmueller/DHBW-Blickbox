758f33243dea1ba64935c77c1afccaf252807dfb
Aron Seidl <aron.seidl@zeiss.com>
2024-09-14 14:38:20 +0200

    #415-Code-Kommentare-auf-deutsch-überarbeitet

--- server/deployment/redis_microservice/RedisMircoservie.py
+++ server/deployment/redis_microservice/RedisMircoservie.py
@@ -9,7 +9,7 @@ import websockets
 import os
 from datetime import datetime, timedelta
 
-
+# Online Status der Komponenten
 onlinestatus = {
                 "Valentin-Online" : {"online" : False, "timestamp" : "1970-1-1 00:00:00"}, 
                 "Grafana-Online" : {"online" : False, "timestamp" : "1970-1-1 00:00:00"}, 
@@ -18,37 +18,37 @@ onlinestatus = {
                 "SARA-Online": {"online" : False, "timestamp" : "1970-1-1 00:00:00"}
                 }
 
-
+# Umgebungsvariablen die Wichtig sind
 link = os.getenv("EMAIL_SMTP_SERVER")
 port = os.getenv("EMAIL_SMTP_PORT")
 password = os.getenv("EMAIL_SMTP_PASSWORD")
 
 
 
-r = redis.Redis(host='redis', port=6379, db=0)
-p = r.pubsub()
-logging.getLogger('websockets').setLevel(logging.ERROR)
-logging.getLogger('asyncio').setLevel(logging.ERROR)
-logging.basicConfig(filename='./logdaten/system_logs.log', level=logging.INFO)
+r = redis.Redis(host='redis', port=6379, db=0)                                                          # Verbindung zur Redis-Datenbank
+p = r.pubsub()                                                                                          # Publisher/Subscriber System von Redis
+logging.getLogger('websockets').setLevel(logging.ERROR)                                                 # Fehler aus der Websocket Libary werden unterdrückt (es sollen nur unsere Logs angezeigt werden)
+logging.getLogger('asyncio').setLevel(logging.ERROR)                                                    # Fehler aus der AsyncIO Libary werden unterdrückt   (es sollen nur unsere Logs angezeigt werden)
+logging.basicConfig(filename='./logdaten/system_logs.log', level=logging.INFO)                          # Logs werden zusätzlich in ein Log-File geschrieben
 
-p.subscribe(["api-logs", "valentin-logs", "grafana-logs", "ada-logs", "sara-logs", "influx-logs"])
+p.subscribe(["api-logs", "valentin-logs", "grafana-logs", "ada-logs", "sara-logs", "influx-logs"])      # Die Channel der Jeweiligen Komponenten im Gesamt-System
 
-publisherList = ["backend","valentin",  "ada", "sara", "grafana", "influx"]
+publisherList = ["backend","valentin",  "ada", "sara", "grafana", "influx"]                             # Die Channel über die Online und Restart Abfragen gemacht werden
 
 
-connected_clients = []
-onlinestatus_connected_clients = []
+connected_clients = []                                                                                  # Offene Frontend Connections
+onlinestatus_connected_clients = []                                                                     # Offene Frontend Connections
 
+# Eingehende Verbindungen werden in die Liste geschrieben & Eingehende Nachrichten werden Ignoriert (nur der Microservice Sendet)
 async def websocket_server(websocket, path):
     connected_clients.append(websocket)
     try:
         async for message in websocket:
             pass  
     finally:
-
         connected_clients.remove(websocket)
 
-
+# Eingehende Verbindungen werden in die Liste geschrieben & Eingehende Nachrichten werden Ignoriert (nur der Microservice Sendet)
 async def onlinestatus_server(websocket, path):
     onlinestatus_connected_clients.append(websocket)
     try:
@@ -57,51 +57,58 @@ async def onlinestatus_server(websocket, path):
     finally:
         onlinestatus_connected_clients.remove(websocket)
 
+# Main Funktion
 async def redis_listener():
-    start_server = websockets.serve(websocket_server, "0.0.0.0", 5001)
+    start_server = websockets.serve(websocket_server, "0.0.0.0", 5001)                          # Websocket für die Logs wird geöffnet
     await start_server
-    onlinestatus_start_server = websockets.serve(onlinestatus_server, "0.0.0.0", 5002)
+    onlinestatus_start_server = websockets.serve(onlinestatus_server, "0.0.0.0", 5002)          # Websocket für die zuletzt Online Daten wird geöffnet
     await onlinestatus_start_server
     
-    asyncio.create_task(aliveChecker())
+    asyncio.create_task(aliveChecker())                                                         # Theread für den Alive Checker wird gestartet
 
-    while True:
-        message = p.get_message()
+    while True: 	                                                                            # (Hauptprogramm) Channel von Redis werden alle abgehört
+        message = p.get_message()                                                               # Wenn eine Nachricht reinkommt wird diese verarbeitet
         if message and message['type'] == 'message':
             await handleLogs(message)
         await asyncio.sleep(0.01) 
 
+# Eingehende Nachrichten werden hier verarbeitet
 async def handleLogs(message):
-    channel = message['channel'].decode('utf-8')
-    data = json.loads(message['data'])
-    if data['type'] == 'info' or data['type'] == 'success':
-        logging.info(beautifyLog(channel,data))
-        await send_logs_to_clients(data)
-    elif data['type'] == "error":
-        errorHandling(channel, data)
-        await send_logs_to_clients(data)
-    elif data['type'] == "online":
-        handleOnlineStatus(channel, data)
+    channel = message['channel'].decode('utf-8')                                                # Source Channel
+    data = json.loads(message['data'])                                                          # Daten werden Extrahiert
     
-
+    if data['type'] == 'info' or data['type'] == 'success':                                     # Es handelt sich um einen normalen Log 
+        logging.info(beautifyLog(channel,data))                                                 # Log wird in eine Datei geschrieben
+        await send_logs_to_clients(data)                                                        # Log wird über den Websocket an das Frontend geschickt
+    
+    elif data['type'] == "error":                                                               # Es handelt sich um einen Fehler
+        errorHandling(channel, data)                                                            # Error Handling wird durchgeführt
+        await send_logs_to_clients(data)                                                        # Log wird über den Websocket an das Frontend geschickt
+    
+    elif data['type'] == "online":                                                              # Es handelt sich um einen Online Request
+        handleOnlineStatus(channel, data)                                                       # Online Status wird aktualisiert
+    
+# Logs werden an jedes FrontEnd verteilt
 async def send_logs_to_clients(data):
     if connected_clients:
         message = json.dumps(data)
         await asyncio.gather(*[client.send(message) for client in connected_clients])
 
+# Online Status wird an jedes FrontEnd verteilt
 async def send_online_status(data):
     if onlinestatus_connected_clients:
         message = json.dumps(data)
         await asyncio.gather(*[client.send(message) for client in onlinestatus_connected_clients])
 
+# Hier passiert Error Handling
 def errorHandling(channel, data):
-    logging.error(beautifyLog(channel,data))
-    heading = f"Komponente {channel.replace('-logs', '')} hat einen fehler"
-    if channel != 'api-logs':
-        sendEmail(heading, data["message"])
+    logging.error(beautifyLog(channel,data))                                                    # Fehlernachricht wird auf die Platte geschrieben
+    heading = f"Komponente {channel.replace('-logs', '')} hat einen fehler"                     # Heading für die Email wird anhand des Channels zusammengebaut (-logs wird entfernt)
+    if channel != 'api-logs':                                                                   # Mich nerven die API Fehler, deswegen da keine Emails, sonst gerne
+        sendEmail(heading, data["message"])                                                     # Email wird mit Fehler nachricht verschickt
+    handleRestart(channel)                                                                      # Neustart wird Initialisiert
 
-    handleRestart(channel)
-
+# In der Funktion wird an den jeweiligen Channel ein Neustart request verschickt
 def handleRestart(channel):
     if channel == 'valentin-logs':
         r.publish('valentin', json.dumps({"request": "restart"}))
@@ -114,6 +121,7 @@ def handleRestart(channel):
     elif channel == 'influx-logs':
         r.publish('influx', json.dumps({"request": "restart"}))
 
+# Der Online Status wird hier aktualisiert
 def handleOnlineStatus(channel, data):
     global onlinestatus
     if channel == 'valentin-logs':
@@ -131,17 +139,16 @@ def handleOnlineStatus(channel, data):
     elif channel == 'influx-logs':
         onlinestatus['Database-Online']['online'] = True
         onlinestatus['Database-Online']['timestamp'] = data['timestamp']
-
     if channel == "api-logs":
         return
-    for item in publisherList:
-        r.publish(item, "Restart")
 
 
+# Logs werden etwas lesbarer aufgehübscht
 def beautifyLog(channel, data):
     message = f"  {data['timestamp']}   {channel}: {data['message']}"
     return message
 
+# Email wird gesendet
 def sendEmail(subject, body):
     senderEmail = 'services@maytastix.de'
     receiver_email = 'aronseidl17@gmail.com'
@@ -157,32 +164,32 @@ def sendEmail(subject, body):
         server.sendmail(senderEmail, receiver_email, message.as_string())
 
 
+# Der Thread fragt alle 2 Minuten ob alle Systeme Online sind
 async def aliveChecker():
     while True:
-        await checkAliveness()
-        await asyncio.sleep(60)
-        await update_online_status()
-        payload = {"Grafana-Online": onlinestatus['Grafana-Online']['online'],
+        await checkAliveness()                                                                  # Anfrage an alle Komponenten ob sie online sind
+        await asyncio.sleep(60)                                                                 # Es wird eine Minute gewarten dass die Systeme Zeit haben zu antworten
+        await update_online_status()                                                            # Online Status wird geupdatet
+        payload = {"Grafana-Online": onlinestatus['Grafana-Online']['online'],                  # Payload fürs Fronend wird aufbereitet
                    "Database-Online": onlinestatus['Database-Online']['online'],
                    "ADA-Last-Online":onlinestatus['ADA-Online']['timestamp'],
                     "SARA-Last-Online" : onlinestatus['SARA-Online']['timestamp'] }
-        await send_online_status(payload)
-        await asyncio.sleep(60)
+        await send_online_status(payload)                                                       # Online Status wird an jedes Fronend geschickt
+        await asyncio.sleep(60)                                                                 # Es wird wieder eine Minute gewartet
+
 
 async def update_online_status():
     global onlinestatus
-    now = datetime.now()
+    now = datetime.now()                                                                        # Momentane Zeit wird abgefragt
+    time_difference = timedelta(minutes=20)                                                     # 20 Minuten unterschied
     
-    time_difference = timedelta(minutes=20)
-    
-    for key, value in onlinestatus.items():
-        timestamp = datetime.strptime(value["timestamp"], "%Y-%m-%d %H:%M:%S")
-        
-        if now - timestamp > time_difference:
+    for key, value in onlinestatus.items():                                                     # Zeitstempel aller Komponenten wird abgefragt
+        timestamp = datetime.strptime(value["timestamp"], "%Y-%m-%d %H:%M:%S")                  
+        if now - timestamp > time_difference:                                                   # Wenn der Letzte Wert länger als 20 Minuten her ist, wird online Status aktualisiert
             onlinestatus[key]["online"] = False
 
-async def checkAliveness():
-    for channel in publisherList:
+async def checkAliveness():                                                                     
+    for channel in publisherList:                                                               # Online Request an jede Komponente
         r.publish(channel,json.dumps({"request": "online"}))
 
-asyncio.run(redis_listener())
+asyncio.run(redis_listener())                                                                   # Hauptprogramm wird ausgeführt
